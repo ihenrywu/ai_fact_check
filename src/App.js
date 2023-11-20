d21392849b8ec5352033509edb56f51854e8bbff
Henry Wu <henrywu.exe@gmail.com>
2023-11-19 20:47:01 -0800

    update

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          AI事实核查员
+        </p>
+        <a
+          className="App-link"
+          href="https://chat.openai.com/g/g-pAwDFQj1H-aishi-shi-he-cha-yuan"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          点我
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
